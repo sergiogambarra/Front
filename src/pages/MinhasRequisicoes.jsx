126b78034dcc35328b9562c054938ad7d4cbcbb3
unknown <jader.mmoura@gmail.com>
2020-03-25 10:55:33 -0300

    alterações na tela visualizar requisicoes escondendo formulario

--- src/pages/MinhasRequisicoes.jsx
+++ src/pages/MinhasRequisicoes.jsx
@@ -11,27 +11,44 @@ class MinhasRequisicoes extends Component {
     super();
     this.state = {
       id: "",
-      dataRequisicao:"",
+      dataRequisicao: "",
       requisições: "", escolha: "",
       pesquisaRequisicoes: [],
-      texto: false, 
+      texto: false,
       disciplinaSolicitada: [
-        this.state={
-          nome:""
+        this.state = {
+          nome: ""
         }
       ],
-      deferido:""
+      deferido: "",
+      cont: ""
     }
   }
+
   listarRequisicoesNome() {
+    
     axios.get(`/api/requisicoes/nome/${this.state.nomeDisciplina}`).then((retorno) => {
-    this.setState({
-      pesquisaRequisicoes:retorno.data
-    })
+      this.setState({
+        pesquisaRequisicoes: retorno.data,
+      })
+      /*if para mostrar lista pesquisa por nome disciplina apagar lista*/
+      /*if (typeof this.state.nomeDisciplina === "undefined") {
+        this.setState({ texto: true })
+      }*/
+      if (this.state.pesquisaRequisicoes.length !== 0) {
+        this.setState({
+          cont: "1", requisições: ""
+        })
+      } else { this.setState({ cont: "" }) }
     });
   }
-  componentDidMount() {
-    this.listarRequisicoesNome()
+  
+  limpar() {
+    this.setState({
+      requisições: "",
+      cont: "",
+      texto:false,nomeDisciplina:""
+    })
   }
   render() {
 
@@ -42,19 +59,19 @@ class MinhasRequisicoes extends Component {
         <div class="custom-control custom-radio custom-control-inline">
           <input type="radio"
             id="todasRequisições" name="customRadioInline1" class="custom-control-input"
-            onChange={(e) => this.setState({ requisições: e.target.id })}
+            onChange={(e) => this.setState({ requisições: e.target.id, cont: "" })}
           />
           <label class="custom-control-label" for="todasRequisições">Todas requisições</label>
         </div>
         <div class="custom-control custom-radio custom-control-inline">
           <input type="radio" id="aproveitamento" name="customRadioInline1" class="custom-control-input"
-            onChange={(e) => this.setState({ requisições: e.target.id })}
+            onChange={(e) => this.setState({ requisições: e.target.id, cont: "" })}
           />
           <label class="custom-control-label" for="aproveitamento">Aproveitamento de estudos</label>
         </div>
         <div class="custom-control custom-radio custom-control-inline">
           <input type="radio" id="certificacao" name="customRadioInline1" class="custom-control-input"
-            onChange={(e) => this.setState({ requisições: e.target.id })} />
+            onChange={(e) => this.setState({ requisições: e.target.id, cont: "" })} />
           <label class="custom-control-label" for="certificacao">Certificação de conhecimentos</label>
         </div>
         <br /><br />
@@ -73,9 +90,10 @@ class MinhasRequisicoes extends Component {
           </select>
           <SACEInput
             placeholder={'Digite aqui sua pesquisa'}
+            value={this.state.nomeDisciplina}
             onChange={(e) => this.setState({ nomeDisciplina: e.target.value })}
             onError={this.state.texto}
-            onErrorMessage={'Campo da disciplina obrigatório'}
+            onErrorMessage={'Não encontrado'}
           />
           {this.state.id === "nomeDisciplina" ? <Button style={{ position: 'relative' }} variant="primary" className="btn btn-primary m-1" onClick={(e) => this.listarRequisicoesNome()}>
             Enviar
@@ -84,38 +102,38 @@ class MinhasRequisicoes extends Component {
                 </Button>}
 
           <Button style={{ position: 'relative' }} variant="danger" className="btn btn-primary m-1"
-            onClick={(e) => this.setState({ requisições: e.target.id })}>
+            onClick={(e) => this.limpar()}
+          > Limpar </Button>
 
-            Limpar
-                </Button>
-          {console.log(this.state.id)}
+
         </div>
-        {<div> <br /><br /><br />
-          <h3>Lista de Requisições por nome da disciplina </h3>
-          <table class="table">
-            <thead class="p-3 mb-2 bg-primary text-white">
-              <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Data</th>
-                <th scope="col">Deferido</th>
-                <th scope="col">Disciplina</th>
+        {this.state.cont === "" ? "" :
+          <div id="FormPesquisaNome" Style={{ display: "none" }}><br /><br /><br />
+            <h3>Lista de Requisições por nome da disciplina </h3>
+            <table class="table" >
+              <thead class="p-3 mb-2 bg-primary text-white">
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Data</th>
+                  <th scope="col">Deferido</th>
+                  <th scope="col">Disciplina</th>
 
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.pesquisaRequisicoes &&
-                this.state.pesquisaRequisicoes.map((requisicao) =>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.pesquisaRequisicoes &&
+                  this.state.pesquisaRequisicoes.map((requisicao) =>
 
-                  <tr>
-                    <td>{requisicao.id}</td>
-                    <td>{requisicao.dataRequisicao}</td>
-                    <td>{requisicao.deferido+""}</td>
-                    <td>{requisicao.disciplinaSolicitada.nome}</td>
+                    <tr>
+                      <td>{requisicao.id}</td>
+                      <td>{requisicao.dataRequisicao}</td>
+                      <td>{requisicao.deferido + ""}</td>
+                      <td>{requisicao.disciplinaSolicitada.nome}</td>
 
-                  </tr>
-                )}
-            </tbody>
-          </table></div>}
+                    </tr>
+                  )}
+              </tbody>
+            </table></div>}
         {this.state.requisições === "" ? "" : this.state.requisições === "todasRequisições" ? <div className="d-flex">
           <div className="col-6">
             <br />
