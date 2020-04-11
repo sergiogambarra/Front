e622b2b28199e92e7a2016ebb02e19e84c4fea7f
Jader Moraes de Moura <jader.mmoura@gmail.com>
2020-04-11 12:37:14 -0300

    feito front mostrar disciplina por curso, corrigir está dando nullPointer

--- src/Cursos/ListaDisciplina.jsx
+++ src/Cursos/ListaDisciplina.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import SACEInput from '../components/inputs/SACEInput';
 import { Button } from 'react-bootstrap';
-
+import { get,del } from '../services/ServicoCrud';
 /*exemplo  style={{display:"none"}}
 */
 
@@ -12,58 +11,47 @@ class ListaDiscipinas extends Component {
 
         super();
         this.state = {
-            texto:false,
-            curso: [
-                this.state = {
-                    idCurso: "",
-                    nome: ""
-                }
-            ],
-
-            disciplinas: [
-                this.state = {
+            texto: false,
+            curso: {
+                id : "",
+                nome: ""
+            }
+            ,
+            disciplinas: {
                     id: "",
                     nome: "",
                     cargaHoraria: ""
                 }
-            ]
         }
     }
 
     listarDisciplina() {
-
-        axios.get(`/api/cursos/${this.state.curso.id}/disciplinas`).then((retorno) => {
-            this.setState({
-                disciplinas: retorno.data
-            })
+        
+        get(`/cursos/${this.state.curso}/disciplinas`).then((retorno) => {
+            this.setState({ disciplinas: retorno.data })
         });
     }
     limpar() {
 
         this.setState({
             nome: "",
-            texto:false
+            texto: false
         });
-       
-    }
 
+    }
+    
     listarCursoNome() {
-        console.log(this.state.nome)
-        if (this.state.nome === ""|| typeof this.state.nome === "undefined") {
-            this.setState({
-                texto: true
-            })
+        if (this.state.nome === "" || typeof this.state.nome === "undefined") {
+            this.setState({ texto: true })
         }
-        axios.get(`/api/cursos/pesquisar/nome/${this.state.nome}`).then((retorno) => {
-            this.setState({
-                curso: retorno.data,
-            })
-            this.listarDisciplina()
-          
+        
+        get(`cursos/pesquisar/nome/${this.state.nome}`).then((retorno) => {
+            this.setState({ curso: retorno })
+            console.log(this.state.curso);
         });
     }
     apagar(e) {
-        axios.delete(`/api/cursos/${this.state.curso.id}/disciplinas/${e}`).then(() => {
+        del(`/api/cursos/${this.state.curso.id}/disciplinas/${e}`).then(() => {
             this.listarCursoNome()
         })
     }
@@ -79,7 +67,7 @@ class ListaDiscipinas extends Component {
         };
 
         return (
-            <div > 
+            <div >
                 <h2>Pesquisar disciplinas pelo nome do curso</h2>
                 <SACEInput
                     placeholder={'Digite o nome do curso que deseja ver as Diciplinas'}
@@ -90,7 +78,7 @@ class ListaDiscipinas extends Component {
                     onErrorMessage={'Nome do curso não encontrado'}
                 />
                 <Button style={{ position: 'relative', left: '80%' }} variant="primary" className="btn btn-primary m-1"
-                 onClick={() => this.listarCursoNome()}>
+                    onClick={() => this.listarCursoNome()}>
                     Enviar
                 </Button>
                 <Button style={{ position: 'relative', left: '80%' }}
@@ -110,21 +98,17 @@ class ListaDiscipinas extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.disciplinas &&
-                            this.state.disciplinas.map((disciplina) =>
+                        {this.state.curso.body&&this.state.curso.body.map((c)=>
+                        <tr>
+                        <td >{c.id}</td>
+                        <td >{c.nome}</td>
+                        <td >{c.cargaHoraria}</td>
+                        <td ><Button 
+                        variant="primary"
+                        className="btn btn-danger m-1">apagar</Button></td>
 
-                                <tr>
-                                    <td>{disciplina.id}</td>
-                                    <td>{disciplina.nome}</td>
-                                    <td>{disciplina.cargaHoraria}</td>
-                                    <td> {disciplina.nome === "" ? "" : <Button
-                                        variant="primary"
-                                        className="btn btn-danger m-1"
-                                        onClick={(e) => this.apagar(disciplina.id)}
-                                    > Deletar </Button>}
-                                    </td>
-                                </tr>
-                            )}
+                    </tr>
+                        )}
                     </tbody>
                 </table>
 
