c341e19a1b12ebb54a7f5cb91760fdac88c22a1b
unknown <jader.mmoura@gmail.com>
2020-05-19 12:08:08 -0300

    começei fazer paginação requisição aluno

--- src/pages/Aluno/RequisicaoAluno.jsx
+++ src/pages/Aluno/RequisicaoAluno.jsx
@@ -5,26 +5,38 @@ class RequisicaoAluno extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           aluno:{},
-           requisicoes:[],
-           mostraTable:false
+            aluno: {},
+            requisicoes: [],
+            mostraTable: false,
+            page: 0,
+            last: false,
+            first: true,
+            total: 0
         }
     }
 
-   async pesquisarNomeSolicitante() {
-        const aluno =  await get(`usuarios/auth/`); 
-        const requisicoes = await get(`requisicoes/solicitante/${aluno.id}`);            
-        this.setState({requisicoes,aluno,mostraTable:true});
+    async pesquisarNomeSolicitante() {
+        const aluno = await get(`usuarios/auth/`);
+        get(`requisicoes/solicitante/${aluno.id}`).then((retorno) => {
+            this.setState({ requisicoes: retorno, last: retorno.last, first: retorno.first, total: retorno.totalPages, aluno, mostraTable: true });
+        })
     }
 
     componentDidMount() {
         this.pesquisarNomeSolicitante()
     }
+    control(e) {
+        if (e.target.id === "+") {
+            this.setState({ page: this.state.page + 1, mostraEditar: false }, () => this.pesquisarNomeSolicitante())
+        } else {
+            this.setState({ page: this.state.page - 1, mostraEditar: false }, () => this.pesquisarNomeSolicitante())
+        }
+    }
     render() {
         return (
             <div>
-                 { this.state.mostraTable && <><br/><br/>
-                    <h3>Requisições do Aluno: {this.state.aluno&&this.state.aluno.perfil.nome}</h3>
+                {this.state.mostraTable && <><br /><br />
+                    <h3>Requisições do Aluno: {this.state.aluno && this.state.aluno.perfil.nome}</h3>
                     <table className="table" >
                         <thead className="p-3 mb-2 bg-primary text-white">
                             <tr >
@@ -32,7 +44,7 @@ class RequisicaoAluno extends Component {
                                 <th scope="col">Data</th>
                                 <th scope="col">Tipo de requisição</th>
                                 <th scope="col">Disciplina</th>
-                                <th style={{textAlign:"center"}} scope="col">Status</th>
+                                <th style={{ textAlign: "center" }} scope="col">Status</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -42,18 +54,28 @@ class RequisicaoAluno extends Component {
                                         <td>{u.id}</td>
                                         <td>{u.dataRequisicao}</td>
                                         <td>{u.tipo === "aproveitamento" ? "Aproveitamentos de estudos" : "Certificação de conhecimentos "}</td>
-                                        <td>{u.disciplinaSolicitada&&u.disciplinaSolicitada.nome}</td>
-                                        <td style={{backgroundColor:
-                                            u.deferido === "DEFERIDO" ? 'green' : u.deferido === "INDEFERIDO" ? 'red' : 'orange'
-                                            ,textAlign:"center"}}>
+                                        <td>{u.disciplinaSolicitada && u.disciplinaSolicitada.nome}</td>
+                                        <td style={{
+                                            backgroundColor:
+                                                u.deferido === "DEFERIDO" ? 'green' : u.deferido === "INDEFERIDO" ? 'red' : 'orange'
+                                            , textAlign: "center"
+                                        }}>
                                             {u.deferido}
                                         </td>
                                     </tr>
                                 )}
                         </tbody>
-                    </table> </>
+                    </table> </>}
+                {
+                    <>
+                        {this.state.first || <button id="-" onClick={(e) => this.control(e)}>Anterior</button>}
+                        &nbsp;&nbsp;
+                        {this.state.last || <button id="+" onClick={(e) => this.control(e)}>Próximo</button>}
 
-                            }
+                        <span style={{ float: "right" }}>Página  {this.state.page + 1} / {this.state.total}</span>
+                    </>
+
+                }
             </div>
 
         );
