a6adf07322dc23b27babe087b40fe247c00a1a1d
unknown <jader.mmoura@gmail.com>
2020-09-23 22:08:36 -0300

    corrigido erro nos importadores

--- src/components/Importador/ImportadorDisciplinas.jsx
+++ src/components/Importador/ImportadorDisciplinas.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
 import { Alert } from 'react-bootstrap';
+import axios from 'axios';
 
 
 class ImportadorDisciplinas extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            alert: "Importado com sucesso",
+            showAlert: false
         }
     }
 
     async enviar() {
-
+        const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+        const form = new FormData();
+        form.append("umArquivo", this.state.umArquivo);
+        axios.post("http://localhost:8080/api/anexos/disciplinas", form, config).then(() => {
+            this.setState({ showAlert: true })
+        })
+        setTimeout(() => {
+            this.setState({showAlert: false})
+        }, 3000);
     }
 
 
@@ -19,15 +29,13 @@ class ImportadorDisciplinas extends Component {
 
         return (
             <div>
-                 <br />  <br />  <br />
+                <br />  <br />  <Alert variant={"success"} show={this.state.showAlert}>{this.state.alert}</Alert> <br />
                 <h3>Importador para cadastro de Disciplinas</h3>
                 <br />
                 <Alert variant={"danger"}>Para importação correta do arquivo com extensão .csv deve estar organizado de modo que, a primeira coluna inclua o nome da disciplina e a segunda coluna inclua a carga horária.</Alert>
                 <br /> <br />
-                <form action={("http://localhost:8080/api/anexos/disciplinas")} method="post" enctype="multipart/form-data" >
-                    <input type="file" name="umArquivo" />
-                    <input type="submit" value="enviar" />
-                </form>
+                <input type="file" name="umArquivo" onChange={(e) => this.setState({ umArquivo: e.target.files[0] })} />
+                <input type="button" value="enviar" onClick={() => this.enviar()} />
 
             </div>
 
--- src/components/Importador/ImportadorUsuarios.jsx
+++ src/components/Importador/ImportadorUsuarios.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Alert } from 'react-bootstrap';
+import axios from 'axios';
 
 
 
@@ -7,12 +8,22 @@ class ImportadorProfessores extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            umArquivo: [],
+            alert: "Importado com sucesso",
+            showAlert: false
         }
     }
 
-    async enviar() {
-
+    enviar(e) {
+        const config = { headers: { 'Content-Type': 'multipart/form-data' } };
+        const form = new FormData();
+        form.append("umArquivo", this.state.umArquivo);
+        axios.post("http://localhost:8080/api/anexos/usuarios", form, config).then(() => {
+            this.setState({ showAlert: true })
+        })
+        setTimeout(() => {
+            this.setState({showAlert: false})
+        }, 3000);
     }
 
 
@@ -20,20 +31,20 @@ class ImportadorProfessores extends Component {
 
         return (
             <div>
-              <br />  <br />  <br />
+                <br />  <br /> <Alert variant={"success"} show={this.state.showAlert}>{this.state.alert}</Alert>  <br />
+
                 <h3>Importador para cadastro de Usuários</h3>
                 <br />
                 <Alert variant={"danger"}>Para importação correta do arquivo com extensão .csv deve estar organizado de modo que:<br />
                  a primeira coluna inclua o nome do usuário;<br />
                  a segunda coluna inclua o número do SIAPE;<br />
                  terceira coluna endereço do email;<br />
-                 quarta coluna se digitar SIM ou NÂO para coordenador de curso;<br />
+                 quarta coluna se digitar SIM ou NÃO para coordenador de curso;<br />
                  quinta coluna o cargo do usuário (PROFESSOR ou SERVIDOR)</Alert>
                 <br /> <br />
-                <form action={("http://localhost:8080/api/anexos/usuarios")} method="post" enctype="multipart/form-data" >
-                    <input type="file" name="umArquivo" />
-                    <input type="submit" value="enviar" />
-                </form>
+
+                <input type="file" name="umArquivo" onChange={(e) => this.setState({ umArquivo: e.target.files[0] })} />
+                <input type="button" value="enviar" onClick={() => this.enviar()} />
 
             </div>
 
